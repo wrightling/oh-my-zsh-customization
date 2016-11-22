599a725c36bdd4cd7b1e8896dcb490541528b143
Warren Wright <wwright@hqwwright1945.local>
2016-11-22 08:29:25 -0800

    set up custom git aliases

---
+++ aliases/git/git.zsh
@@ -0,0 +1,13 @@
+# Use `hub` as our git wrapper:
+#   http://defunkt.github.com/hub/
+hub_path=$(which hub)
+if (( $+commands[hub] ))
+then
+  alias git=$hub_path
+fi
+
+alias gca='git add -A . && git commit'
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+alias gone="git log --oneline --decorate"
+alias gp='git push origin HEAD'
+alias gs='git status -sb'
