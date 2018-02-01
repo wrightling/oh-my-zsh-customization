08ee8357985d7cd1516f0854881a65152cc16cc9
Warren Wright <warren@thewrightangle.com>
2018-02-01 14:22:06 -0700

    Fix custom aliases to load all alias zsh files, not just the first one found

--- aliases.zsh
+++ aliases.zsh
@@ -1,5 +1,11 @@
-source $ZSH_CUSTOM/aliases/**/*.zsh
+# source $ZSH_CUSTOM/aliases/**/*.zsh
+typeset -ga sources
+sources=($ZSH_CUSTOM/aliases/**/*.zsh)
+
+foreach file (`echo $sources`)
+    if [[ -a $file ]]; then
+        source $file
+    fi
+end
 
 alias reload!='source ~/.zshrc && echo Reloaded .zshrc'
-
-alias dc='docker-compose -f /Users/wwright/Dev/stella/rosi_compose/docker-compose.dev.yml'
