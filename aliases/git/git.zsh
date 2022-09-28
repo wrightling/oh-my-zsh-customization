9648b72ee9e7b6a9f604cb0719aa8cf1a1465ee3
warren <warren@thewrightangle.com>
2022-09-28 12:08:09 -0600

    add gshort alias

--- aliases/git/git.zsh
+++ aliases/git/git.zsh
@@ -9,6 +9,7 @@ fi
 alias gca='git add -A . && git commit'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gone="git log --oneline --decorate"
+alias gshort="git log --pretty=format:'%C(auto)%h %an%x09%Cgreen%ad %C(auto)%d %s' --date=short"
 alias gp='git push origin HEAD'
 alias gs='git status -sb'
 alias gda='gitdiffall'
