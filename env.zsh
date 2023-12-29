612d456f1795a82ead7d738f7f5ec298dea5aa27
Warren Wright <warren@thewrightangle.com>
2023-12-29 14:50:28 -0700

    remove some rbenv-specific lines from env.zsh

--- env.zsh
+++ env.zsh
@@ -1,13 +1,13 @@
 export PATH="$PATH:/usr/local/go/bin"
 export PROJECTS="$HOME/Dev"
 
-if uname -r | grep -q -i -v 'Microsoft' ; then
-  export PATH="$HOME/.rbenv/bin:$PATH"
-  eval "$(rbenv init -)"
-
-  export PATH="$PATH:/Applications:$HOME/bin"
-else
-fi
+# if uname -r | grep -q -i -v 'Microsoft' ; then
+#   export PATH="$HOME/.rbenv/bin:$PATH"
+#   eval "$(rbenv init -)"
+# 
+#   export PATH="$PATH:/Applications:$HOME/bin"
+# else
+# fi
 
 # required for VIM to pick up cursor changes on windows-terminal
 echo -ne "\e[2 q"
